72cb786ed1fb05e012c7f526dfb3852403855b06
Brent Edwards <bedwards@inmusicbrands.com>
2024-02-08 19:26:22 +1300

    Chapter 1 - Initial Commit

---
+++ auth-tutorial/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <>
+    <p className="font-semibold text-green-500">Hello Auth</p>
+    <Button
+    size="xlg"
+    >Click Me</Button>
+    </>
+  )  
+}
