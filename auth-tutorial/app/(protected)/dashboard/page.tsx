720367c5396f5290466da13658c563d0905907e3
Brent Edwards <bedwards@inmusicbrands.com>
2024-02-12 21:47:38 +1300

    Chapter 12 - Dashboard

--- auth-tutorial/app/(protected)/dashboard/page.tsx
+++ auth-tutorial/app/(protected)/dashboard/page.tsx
@@ -21,45 +21,23 @@ const DashboardPage = async () => {
     <main className="flex h-full flex-col items-center justify-center 
     bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800
     ">
-    <div className="space-y-6 text-center">
-      <div className="flex flex-row space-x-2 justify-center">
-        <RiDashboard3Line className="text-6xl font-semibold text-white drop-shadow-md"/>
-        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className)}>Dashboard</h1>
-        {/* <p>{JSON.stringify(session)}</p> */}
+      <div className="space-y-6 text-center">
+        <div className="flex flex-row space-x-2 justify-center">
+          <RiDashboard3Line className="text-6xl font-semibold text-white drop-shadow-md"/>
+          <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className)}>Dashboard</h1>
+        </div>
+        <h1 className="text-lg text-white drop-shadow-md">Name: {session?.user.name}</h1>
+        <h1 className="text-lg text-white drop-shadow-md">Email: {session?.user.email}</h1>  
+        <div>
+          <form action={async ()=>{"use server"; await signOut(); }}>
+            <LoginButton mode="redirect">
+              <Button type="submit" variant="secondary" size="lg">Sign Out</Button>
+            </LoginButton>
+          </form>
+        </div>
       </div>
-      <h1 className="text-lg text-white drop-shadow-md">Name: {session?.user.name}</h1>
-      <h1 className="text-lg text-white drop-shadow-md">Email: {session?.user.email}</h1>  
-      <div>
-        <form action={async ()=>{
-          "use server";
-          await signOut();
-          }}
-        >
-        <LoginButton mode="redirect">
-          <Button type="submit" variant="secondary" size="lg">Sign Out</Button>
-        </LoginButton>
-       </form>
-      </div>
-    </div>
     </main>
-  )};
-
-
-  // return (
-    
-  //   <div>
-  //     <h1>Dashboard Page</h1>
-  //     <p>{JSON.stringify(session)}</p>
-  //     <p>{role}</p>
-  //     <form action={async ()=>{
-  //       "use server";
-  //       await signOut();
-  //     }}>
-  //       <button type="submit">Sign Out</button>
-  //     </form>
-  //     </div>
-
-  // )
-
+  );
+};
 
 export default DashboardPage;
